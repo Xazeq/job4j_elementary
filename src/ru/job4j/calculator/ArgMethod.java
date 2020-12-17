2026a3d78fcbc4ce1df0a83aeb0ebda70cd13083
Xazeq <setezey@gmail.com>
2020-12-17 19:12:58 +0300

    4.1. Аргументы [#433320]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello (String name){
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Nikita";
+        int age = 24;
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+        ArgMethod.hello(name);
+
+    }
+}
