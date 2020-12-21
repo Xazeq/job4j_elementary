112feaa0371ac6b7532696fe31862d8c155c69b5
Xazeq <setezey@gmail.com>
2020-12-21 19:42:21 +0300

    Доработка

--- src/ru/job4j/condition/PairsCharString.java
+++ src/ru/job4j/condition/PairsCharString.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class PairsCharString {
     public static boolean check(String l, String r) {
-        if (l.equals(r) && l.equals("")){
+        if (l.equals(r) && l.equals("")) {
             return true;
         } else if (l.charAt(0) == r.charAt(r.length() - 1) && l.charAt(l.length() - 1) == r.charAt(0)) {
             return true;
