26485eaf3cf062483ee16df70cf37d6998653a67
Xazeq <setezey@gmail.com>
2020-12-21 19:26:12 +0300

    Добавлен класс PairsCharString

---
+++ src/ru/job4j/condition/PairsCharString.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class PairsCharString {
+    public static boolean check(String l, String r) {
+        if (l.equals(r) && l.equals("")){
+            return true;
+        } else if (l.charAt(0) == r.charAt(r.length() - 1) && l.charAt(l.length() - 1) == r.charAt(0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
