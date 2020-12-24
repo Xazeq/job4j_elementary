3adc92c04718814ad5e6973db53ebb08662b7516
Xazeq <setezey@gmail.com>
2020-12-24 19:07:51 +0300

    5.7. Ипотека [#433368]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
-        int year = 1;
-        double debt = amount * percent / 100 + amount;
-        while (debt > salary) {
-            debt = debt * percent / 100 + debt - salary;
+        int year = 0;
+        while (amount > 0) {
+            amount = amount * percent / 100 + amount - salary;
             year++;
         }
         return year;
